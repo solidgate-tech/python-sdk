5b3f8f90703bcd9118694c4b6b8d9d93b94a1e7c
Zarina Madai <zarina.maday@gen.tech>
2021-01-13 09:25:37 +0000

    Change response type for reconciliation methods: dict -> str

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="solidgate-card-sdk",
-    version="0.0.3",
+    version="0.0.5",
     author="SolidGate",
     author_email="info@solidgate.com",
     description="Python API SDK for SolidGate payment gateway",
